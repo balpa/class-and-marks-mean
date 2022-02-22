8596aaec29ce72b59c39d932426ba6bac06aaf76
Berke Altıparmak <44147280+balpa@users.noreply.github.com>
2022-02-22 16:38:15 +0300

    Update classandmarks.py

--- classandmarks.py
+++ classandmarks.py
@@ -1,5 +1,17 @@
-import random
+# @author: Berke Altıparmak
+# You may use, distribute and modify this code under the
+# terms of the Beerware license, which unfortunately won't be
+# written for another century.
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <berkealtiparmak@outlook.com> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return.   Berke Altıparmak
+# ----------------------------------------------------------------------------
+#
 
+
+import random
 
 class Person:
     def __init__(
